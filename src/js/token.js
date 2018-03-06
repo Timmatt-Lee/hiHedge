71e917c12f46fab3203362fc47f428e42ae5d0d9
Timmatt <qpalzm19970904@gmail.com>
2018-03-06 21:23:48 +0800

    1.2.2 Readable & Copyable Address, SideBar UI
    Update:
    1. Attach clipboard(plug-in) to content with address
    2. Apply simpleBar(plug-in) to content with overflow address
    3. Interact tooltips to copyable address
    4. Apply simpleBar to left-side information in Token Tab
    5. Stick left-side information in Token Tab as side bar
    6. Compatize some css with simpleBar
    7. Add tooltip and Block informations for non-login user
    8. Add traders' temp avatar
    
    Future:
    1. Make multi-icons in Trader cards
    2. Complete Trader contract and start interact

--- src/js/token.js
+++ src/js/token.js
@@ -38,28 +38,17 @@ Token = {
 	},
 
 	listeners: function() {
+		// 複製地址按鈕
+		new ClipboardJS('.address-copier');
+
+		// 偵測凍結欄位只要一變動就刷新該帳號凍結資訊
 		$('#token-freeze input').on('keyup', () => { Token.updateFrozenForm(); });
 
-		// 監聽event: Allowance
-		Token.contract.then(() => {
-			// 自己傳送的Allowance
-			Token.instance.Allowance({ from: web3.eth.defaultAccount }, { fromBlock: 0 }, (error, result) => {
-				if (error) {
-					console.error('Token.getMyAllowance', error);
-					return;
-				}
-				Token.myAllowance[result.args.to] = web3.fromWei(result.args.value);
-			});
-			// 被別人允許的Allowance
-			Token.instance.Allowance({ to: web3.eth.defaultAccount }, { fromBlock: 0 }, (error, result) => {
-				if (error) {
-					console.error('Token.getForMeAllowance', error);
-					return;
-				}
-				Token.forMeAllowance[result.args.from] = web3.fromWei(result.args.value);
-			});
-		}).catch((error) =>
-			console.error('Token.Allowance(Event)', error.message));
+		// 掛上複製地址的監聽器
+		addressCopier_listener('.address-copier');
+
+		// 獲得此帳號的	myAllowance、forMeAllowance
+		Token.getApprove();
 	},
 
 	// 要與區塊鏈同步的資料
@@ -77,18 +66,37 @@ Token = {
 	initUI: function() {
 		console.log('Loading: init UI...');
 
-		// Tooltip基本attribute寫入
-		$('.tooltip-notOwner').attr('data-toggle', 'tooltip');
-		$('.tooltip-notOwner').attr('title', 'Need Authentication');
-		$('.tooltip-gotFrozen').attr('data-toggle', 'tooltip');
-		$('.tooltip-gotFrozen').attr('title', 'Sorry, you\'ve got frozen');
+		// 使用者帳戶地址
+		if (web3.eth.defaultAccount === undefined) // 若沒登入
+		{
+			// alert to log in
+			swal('Who are you ?', 'Please log in to your wallet for more', 'warning')
+			// set content with address to '-'
+			$('input.ether-userAddress').val('-');
+			$('.ether-userAddress:not(:input)').text('-');
+			// hide some cards
+			$('.wallet-user, .trader-subscription, .trader-subscriber').hide();
+			// 沒登入就來個提示登入的tooltip吧
+			$('.tooltip-login').tooltip({ title: 'Please log in' });
+			// 把上述的欄位進用
+			$('.tooltip-login *').prop('disabled', true);
+			// 把用戶的Balance欄位都變成0
+			$('.token-userBalance, .ether-userBalance').text(0);
+		} else {
+			$('input.ether-userAddress').val(web3.eth.defaultAccount);
+			$('.ether-userAddress:not(:input)').text(web3.eth.defaultAccount);
+			$('.ether-userAddress').attr('data-clipboard-text', web3.eth.defaultAccount);
+			// 有登入才寫其他Tooltip（凍結、發行者權限檢查）的提示
+			$('.tooltip-notOwner').tooltip({ title: 'Need Authentication' });
+			$('.tooltip-gotFrozen').tooltip({ title: 'Sorry, you\'ve got frozen' });
+		}
+		// 地址複製tooltip，用manual trigger，用addressCopier_listener()裡控制
+		$('.address-copier').tooltip({ title: 'click to copy', trigger: 'manual' });
 		// input後的invalid-tooltip的內文
 		$('input[placeholder*="Address"] + .invalid-tooltip').text('I need a valid address');
 		$('input[placeholder*="Amount"] + .invalid-tooltip, input[placeholder*="ether"] + .invalid-tooltip').text('Come on... give me a positive number');
 
 		Token.contract.then(() => {
-			// 使用者帳戶地址
-			$('.ether-userAddress').text(web3.eth.defaultAccount);
 			// Token符號
 			return Token.instance.symbol();
 
@@ -122,15 +130,21 @@ Token = {
 	},
 
 	updateUI: function() {
-		// 更新Balances
-		$('.token-userBalance').text(Token.userTokenBalance);
+		// minter Balances
 		$('.token-minterBalance').text(Token.minterTokenBalance);
-		$('.ether-userBalance').text(Token.userEtherBalance);
 		$('.ether-minterBalance').text(Token.minterEtherBalance);
 
 		// 更新買賣價
 		$('.token-sellPrice').text(Token.sellPrice);
 		$('.token-buyPrice').text(Token.buyPrice);
+
+		if (web3.eth.defaultAccount === undefined)
+			return;
+		// 要登入才能更新以下內容
+
+		// 用戶Balances
+		$('.token-userBalance').text(Token.userTokenBalance);
+		$('.ether-userBalance').text(Token.userEtherBalance);
 
 		// 若帳戶被凍結
 		$('.tooltip-gotFrozen').tooltip(Token.isFrozen ? 'enable' : 'disable');
@@ -147,10 +161,14 @@ Token = {
 			arr.sort((a, b) => { return b[1] - a[1] });
 			// table UI
 			$(table_id + ' table tbody').empty();
-			$.each(arr, (index, [account, value]) => {
-				$(table_id + ' table > tbody').append('<tr><th scope="row">' + (index + 1) + '</th> <td>' + account + '</td> <td>' + value + '</td></tr>');
-			});
+			$.each(arr, (index, [account, value]) =>
+				$(table_id + ' table > tbody').append('<tr><th scope="row">' + (index + 1) + '</th><td data-simplebar data-toggle="tooltip" class="address-copier" data-clipboard-text="' + account + '">' + account + '</td><td>' + value + '</td></tr>')
+			);
+			// 這邊初始化複製地址的tooltip以及綁定監聽器
+			$(table_id + ' .address-copier').tooltip({ title: 'click to copy', trigger: 'manual' });
+			addressCopier_listener(table_id + ' .address-copier');
 		});
+
 	},
 
 	getBalances: function() {
@@ -176,7 +194,7 @@ Token = {
 		var _toAddress = $('#token-transfer input[placeholder="Address"]').val();
 		var _amount = $('#token-transfer input[placeholder="Amount"]').val();
 
-		var _message = 'transfer ' + _amount + Token.symbol + ' to ' + _toAddress;
+		var _message = 'transfer ' + _amount + ' ' + Token.symbol + ' to ' + _toAddress;
 		console.log('Pending: ' + _message + '...');
 
 		// 檢查傳送帳號是否被凍結
@@ -196,7 +214,7 @@ Token = {
 
 	sell: function() {
 		var _sellAmount = $('#token-sell input').val();
-		var _message = 'sell ' + _sellAmount + Token.symbol;
+		var _message = 'sell ' + _sellAmount + ' ' + Token.symbol;
 		var _soldValue;
 
 		console.log('Pending: ' + _message + '...');
@@ -217,10 +235,10 @@ Token = {
 
 	buy: function() {
 		var _buyValue = $('#token-buy input').val();
-		var _message = ' by ' + _buyValue + 'ether';
+		var _message = ' by ' + _buyValue + ' ether';
 		var _boughtAmount;
 
-		console.log('Pending: buy ' + Token.symbol + _message + '...');
+		console.log('Pending: buy ' + _buyValue + ' ' + Token.symbol + _message + '...');
 
 		Token.contract.then(() => {
 			// 換算成ether
@@ -231,7 +249,7 @@ Token = {
 			return Token.instance.buy({ value: web3.toWei(_buyValue) });
 
 		}).then(() =>
-			swal('Now only wait for mined', 'buy ' + web3.fromWei(_boughtAmount) + Token.symbol + _message, 'success')
+			swal('Now only wait for mined', 'buy ' + web3.fromWei(_boughtAmount) + ' ' + Token.symbol + _message, 'success')
 
 		).catch((error) =>
 			console.error('Token.buy()', error.message));
@@ -298,7 +316,7 @@ Token = {
 		if (_toAddress == '')
 			_toAddress = web3.eth.defaultAccount;
 
-		var _message = 'mint ' + _amount + Token.symbol + ' to ' + _toAddress;
+		var _message = 'mint ' + _amount + ' ' + Token.symbol + ' to ' + _toAddress;
 		console.log('Pending: ' + _message + '...');
 
 		Token.contract.then(() => {
@@ -395,7 +413,7 @@ Token = {
 		if (_toAddress == '')
 			_amount = 0;
 
-		var _message = 'approve ' + _toAddress + ' with ' + _amount + Token.symbol;
+		var _message = 'approve ' + _toAddress + ' with ' + _amount + ' ' + Token.symbol;
 		console.log('Pending: ' + _message + '...');
 
 		// 檢查傳送帳號是否被凍結
@@ -413,6 +431,29 @@ Token = {
 			console.error('Token.approve()', error.message));
 	},
 
+	getApprove: function() {
+		// 監聽event: Allowance
+		Token.contract.then(() => {
+			// 自己傳送的Allowance
+			Token.instance.Allowance({ from: web3.eth.defaultAccount }, { fromBlock: 0 }, (error, result) => {
+				if (error) {
+					console.error('Token.getMyAllowance', error);
+					return;
+				}
+				Token.myAllowance[result.args.to] = web3.fromWei(result.args.value);
+			});
+			// 被別人允許的Allowance
+			Token.instance.Allowance({ to: web3.eth.defaultAccount }, { fromBlock: 0 }, (error, result) => {
+				if (error) {
+					console.error('Token.getForMeAllowance', error);
+					return;
+				}
+				Token.forMeAllowance[result.args.from] = web3.fromWei(result.args.value);
+			});
+		}).catch((error) =>
+			console.error('Token.Allowance(Event)', error.message));
+	},
+
 	transferFrom: function() {
 		var _fromAddress = $('#token-transferFrom input[placeholder="Address from"]').val();
 		var _toAddress = $('#token-transferFrom input[placeholder="Address to"]').val();
@@ -422,7 +463,7 @@ Token = {
 		if (_toAddress == '')
 			_toAddress = web3.eth.defaultAccount;
 
-		var _message = 'transfer ' + _amount + Token.symbol + ' from ' + _fromAddress + ' to ' + _toAddress;
+		var _message = 'transfer ' + _amount + ' ' + Token.symbol + ' from ' + _fromAddress + ' to ' + _toAddress;
 		console.log('Pending: ' + _message + '...');
 
 		// 檢查來源帳號是否被凍結
@@ -465,7 +506,7 @@ Token = {
 	burn: function() {
 		var _amount = $('#token-burn input[placeholder="Amount"]').val();
 
-		var _message = 'burn ' + _amount + Token.symbol;
+		var _message = 'burn ' + _amount + ' ' + Token.symbol;
 		console.log('Pending: ' + _message + '...');
 
 		Token.contract.then(() => {
@@ -482,7 +523,7 @@ Token = {
 		var _fromAddress = $('#token-burnFrom input[placeholder="Address from"]').val();
 		var _amount = $('#token-burnFrom input[placeholder="Amount"]').val();
 
-		var _message = 'burn ' + _amount + Token.symbol + ' from ' + _fromAddress;
+		var _message = 'burn ' + _amount + ' ' + Token.symbol + ' from ' + _fromAddress;
 		console.log('Pending: ' + _message + '...');
 
 		Token.contract.then(() => {
@@ -529,4 +570,45 @@ function checkValidityMacro(_selector, _function) {
 	});
 	// 當focus到其他地方後，檢查就取消
 	$(':not(' + _selector + ' *)').on('focus', () => $(_selector).removeClass('was-validated'));
+}
+
+function addressCopier_listener(_selector) {
+	$(_selector).on({
+		// 以下操作都是針對tooltip
+		'mouseenter': (event) => $(event.delegateTarget).tooltip('show'),
+		'mouseleave': (event) => $(event.delegateTarget).tooltip('hide'),
+		'click': (event) => {
+			var t = $(event.delegateTarget);
+			var tID = t.attr('timeoutID');
+			// 以DOM的屬性判斷這個元素是否還有計時器在跑
+			if (typeof tID === typeof undefined || tID === false) {
+				// 代表目前沒有計時器在跑，那我們就要換文字然後讓他懸停三秒
+				// 先暫時停止監聽滑鼠離開
+				t.off('mouseleave');
+				// 因為要更新文字所以要先暫時隱藏他
+				t.tooltip('hide');
+				// 更新文字
+				t.attr('data-original-title', 'Copied');
+				// 剛剛上面那個暫時隱藏一隱藏完就...
+				t.on('hidden.bs.tooltip', () => {
+					// 當然就是趕快再顯示他，因為此時他已經換好提示文字了
+					t.tooltip('show');
+					// 重要！立馬停止此監聽，因為只要觸發一次這個監聽就沒用了
+					t.off('hidden.bs.tooltip');
+				});
+			} else // 代表之前的計時器還沒停止
+				clearTimeout(tID); // 那就清除前一個計時器，下面會重新計時
+
+			// 讓他懸停三秒，三秒後變回原樣，在這個瞬間把這個計時器的id寫進DOM的屬性
+			t.attr('timeoutID', setTimeout(() => {
+				//　讓他再度隱藏，因為又要換文字
+				t.tooltip('hide');
+				t.attr('data-original-title', 'click to copy');
+				//　別忘了重啟監聽滑鼠移開
+				t.on('mouseleave', (event) => t.tooltip('hide'));
+				// 計時順利完成救把DOM的計時器屬性移除，代表沒有計時器正在運行了
+				t.removeAttr('timeoutID', false);
+			}, 3000));
+		}
+	});
 }
